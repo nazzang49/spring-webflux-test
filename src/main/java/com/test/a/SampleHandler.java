3dbb1010911fcaf3ecb275fbd3deafee98a8fd14
nazzang49 <nazzang49@naver.com>
2022-06-03 23:11:14 +0900

    update : 2-depth flatmap

--- src/main/java/com/test/a/SampleHandler.java
+++ src/main/java/com/test/a/SampleHandler.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
-import java.util.List;
+
+import java.util.HashMap;
 
 /**
  * A handler for sample request - response
@@ -20,11 +21,30 @@ public class SampleHandler {
     private SampleService sampleService;
 
     public Mono<ServerResponse> test(ServerRequest serverRequest) {
-        Mono<List<Integer>> test = sampleService.test();
-        test.subscribe(o -> System.out.println("test : " + o));
+        Mono<HashMap<String, Object>> hashMapMono = sampleService.test4();
+        return sampleService.test()
+                .collectList()
+                .flatMap(o -> {
+                    // trigger as return
+                    return hashMapMono.flatMap(stringObjectHashMap -> {
+                        stringObjectHashMap.put("test-key1", "test-value1");
+                        return Mono.just(stringObjectHashMap);
+                    });
+                })
+                .flatMap(o -> {
+                    return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(o);
+                });
 
-        return ServerResponse.ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(test, List.class);
+//        return sampleService.test()
+//                .flatMap(o -> {
+//                    Mono<List<Integer>> listMono1 = sampleService.test1();
+//                    Mono<List<Integer>> listMono2 = sampleService.test2();
+//                    Mono<List<Integer>> listMono3 = sampleService.test3();
+//                    return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(Mono.just(o), List.class);
+//                });
+
+//        return ServerResponse.ok()
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .body(test, List.class);
     }
 }
--- src/main/java/com/test/a/SampleService.java
+++ src/main/java/com/test/a/SampleService.java
@@ -4,12 +4,34 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Service
 public class SampleService {
 
-    public Mono<List<Integer>> test() {
+    public Flux<Integer> test() {
+        return Flux.range(1, 10);
+    }
+
+    public Mono<List<Integer>> test1() {
+        System.out.println("test1-came-in");
         return Flux.range(1, 10).collectList();
+    }
+
+    public Mono<List<Integer>> test2() {
+        System.out.println("test2-came-in");
+        return Flux.range(1, 10).collectList();
+    }
+
+    public Mono<List<Integer>> test3() {
+        System.out.println("test3-came-in");
+        return Flux.range(1, 10).collectList();
+    }
+
+    public Mono<HashMap<String, Object>> test4() {
+        System.out.println("test4-came-in");
+        HashMap<String, Object> stringObjectHashMap = new HashMap<>();
+        return Mono.just(stringObjectHashMap);
     }
 }
